8c9838dab7cc4e825de5c53b83e06b394da2933a
Ferdan Sarah <sarah.ferdan@htl.rennweg.at>
2019-03-23 20:43:10 +0100

    [TASK] erste Komponente erstellt

---
+++ app/components/r2p.js
@@ -0,0 +1,14 @@
+"use strict";
+
+app.component("r2p", {
+    templateUrl: "components/r2p.html",
+    controller: "R2pController",
+    bindings: {}
+});
+
+
+app.controller("R2pController", function ($log) {
+
+    $log.debug("R2pController()");
+
+});
