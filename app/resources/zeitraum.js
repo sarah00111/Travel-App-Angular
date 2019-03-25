8e79dfe8dcdf0a0cc8524856d72d618cc46b1ee0
Angela Amacna <6031@htl.rennweg.at>
2019-03-25 14:50:58 +0100

    [TASK] Prototyp für Adresse und Zeitraum

---
+++ app/resources/zeitraum.js
@@ -0,0 +1,15 @@
+"use strict";
+
+app.component("zeitraum", {
+    templateUrl: "components/zeitraum.html",
+    controller: "ZeitraumController",
+    bindings: {}
+});
+
+app.controller("ZeitraumController", function ($log) {
+
+    $log.debug("ZeitraumController()");
+
+});/**
+ * Created by User on 24.03.2019.
+ */
