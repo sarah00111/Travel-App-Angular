45a9cfd3ee65373079040e810c117ec3d29a5783
Angela Amacna <6031@htl.rennweg.at>
2019-03-25 15:20:30 +0100

    [TASK] Services schreiben

---
+++ app/models/address.js
@@ -0,0 +1,3 @@
+/**
+ * Created by User on 25.03.2019.
+ */
---
+++ app/services/repository.js
@@ -0,0 +1,22 @@
+/**
+ * Created by User on 25.03.2019.
+ */
+
+"use strict";
+
+app.service("RespositoryService", function ($log, Farbe) {
+
+    $log.debug("RespositoryService()");
+
+    this.rep = [];
+
+    function getRep() {
+        return this.rep;
+    }
+
+    function newEintrag(param) {
+        this.push(param);
+    }
+
+});
+
